e5a5c630f46668dbe4e71b8a6558bf8d36cd9611
Guillaume Van de Velde <guillaume.vandevelde@student.hogent.be>
2022-02-06 17:32:50 +0100

    First changes to convert to a PWA

---
+++ gardenapp/src/app/app.component.ts
@@ -0,0 +1,18 @@
+import { Component } from '@angular/core';
+import {environment} from './../environments/environment'
+
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.sass']
+})
+export class AppComponent {
+  
+  constructor(){
+    console.log(environment.production);
+  }
+  
+  title = 'gardenapp';
+
+}
